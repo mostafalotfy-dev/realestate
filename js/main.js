0e7f3985717a0436cb0cb6f331f80cdeb7fcaec6
mostafa lotfy <mostafalotfy285@gmail.com>
2023-07-04 13:20:47 +0300

    add readme

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ import  "bootstrap";
 const $ = require("jquery")
 import "./slick.min";
 
-
+!(function($){
 $('.slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
     dots:true
-  });
-          
+  }
+  );
+}($))
