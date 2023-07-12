a68545a087f3a843a15b7db18a29fe4821155062
mostafa lotfy <mostafalotfy285@gmail.com>
2023-07-12 11:16:12 +0300

    update folder structure

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,10 @@
 const mix = require("laravel-mix");
 
-
 mix
 .sass("fontawesome/scss/main.scss","dist")
-
+.pug("Components/**/*.pug")
 .js("fontawesome/js/main.js","dist")
-.webpackConfig({
-    
-})
+
 .disableSuccessNotifications()
 
 
